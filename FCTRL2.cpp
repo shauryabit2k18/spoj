5e95813adf9e856c6b71ae4f8161eab97284a76e
shauryabit2k18 <shaurya.r.sinha2000@gmail.com>
2019-09-10 00:19:02 +0530

    Update FCTRL2.cpp

--- FCTRL2.cpp
+++ FCTRL2.cpp
@@ -1,14 +1,40 @@
 # include <bits/stdc++.h>
 
 using namespace std;
-int fac(int x)
+
+int multiply(int x, int res[], int res_size);
+
+// This function finds factorial of large numbers
+// and prints them
+void factorial(int n)
 {
-    int a = 1 , i;
-    for(i = x ; i >0 ; i--)
+    int res[500];
+    res[0] = 1;
+    int res_size = 1;
+    for (int x=2; x<=n; x++)
+        res_size = multiply(x, res, res_size);
+
+    for (int i=res_size-1; i>=0; i--)
+        cout << res[i];
+}
+
+int multiply(int x, int res[], int res_size)
+{
+    int carry = 0;
+    for (int i=0; i<res_size; i++)
     {
-        a = a*i;
+        int prod = res[i] * x + carry;
+        res[i] = prod % 10;
+        carry  = prod/10;
     }
-    return a;
+
+    while (carry)
+    {
+        res[res_size] = carry%10;
+        carry = carry/10;
+        res_size++;
+    }
+    return res_size;
 }
 int main()
 {
@@ -18,9 +44,12 @@ int main()
     for (i = 0 ; i < t ; i++)
     {
         cin >> n[i];
+
     }
     for (i = 0 ; i < t ; i++)
     {
-        cout << fac(n[i]) << endl;
+        factorial(n[i]);
+        cout<<"\n";
     }
+    return 0;
 }
