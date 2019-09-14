33d26e8b77279caa990d230bc3b41d6f90b1f8ce
shauryabit2k18 <shaurya.r.sinha2000@gmail.com>
2019-09-14 11:24:19 +0530

    Update LUCKYFOUR.c

--- LUCKYFOUR.c
+++ LUCKYFOUR.c
@@ -1,1 +1,33 @@
+# include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    long int t;
+    cin >> t;
+    while(t--)
+    {
+        long long int n , r , q ;
+        int count = 0;
+        cin >> n;
+        q=n;
+        if(q==4)
+        {
+            count ++;
+        }
+        else
+        {
+            for(int i = 0 ; i<9 ; i++)
+            {
+                r = q%10;
+                if(r == 4)
+                {
+                    count++;
+                }
+                q = q/10;
+            }
+        }
+       cout << count << endl;
+    }
+}
